ba20fd91144e92d55d3b8b69683883aa87f99112
lsemlali-13 <semllmes01@gmail.com>
2024-08-09 18:23:57 +0100

    improved ui and added colors to guessed letters

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 const WORD_SIZE = 5;
 let currentIndex = 0;
 const randomIndex = Math.floor(Math.random() * words.length);
-const wordToGuess = words[randomIndex];
+const wordToGuess = words[randomIndex].toUpperCase();
 console.log(wordToGuess)
 function App() {
   const [guesses, setGuesses] = useState(Array(6).fill(null));
@@ -20,15 +20,16 @@ function App() {
         })
       }
       <input 
+        className='input'
         type='text'value={currentGuess} 
         onChange={(e) => {
           setCurrentGuess(e.target.value);
         }}
         onKeyDown={(e) => {
-          if (e.key == 'Enter') {
-            if (currentGuess.length == 5 && currentGuess.match(/^[a-zA-Z]{5}$/)) {
+          if (e.key === 'Enter') {
+            if (currentGuess.length === 5 && currentGuess.match(/^[a-zA-Z]{5}$/)) {
               let newGuesses = [...guesses];
-              newGuesses[currentIndex] = currentGuess;
+              newGuesses[currentIndex] = currentGuess.toUpperCase();
               setCurrentGuess('');
               setGuesses(newGuesses)
               currentIndex++;
@@ -42,8 +43,17 @@ function App() {
 function Line({ guess }) {
   let char = ''
   let boxes = []
+  let className = 'tile';
   for (let i = 0; i < WORD_SIZE; i++) {
-    char = <div className='box'>{guess[i]}</div>;
+    console.log(guess, '---', wordToGuess)
+    if (guess && wordToGuess[i] === guess[i])
+      className += ' correct';
+    else if (guess && wordToGuess.includes(guess[i]))
+      className += ' close';
+    else if (guess)
+      className += ' incorrect';
+    char = <div className={className}>{guess[i]}</div>;
+    className = 'tile';
     boxes.push(char);
   }
 
